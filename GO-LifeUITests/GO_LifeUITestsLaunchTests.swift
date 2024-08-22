2725ed02544db4474a160b02fcbabe7fd7c18019
Elliot Rapp <elliotmrapp@gmail.com>
2024-08-22 17:59:02 -0500

    Initial Commit

---
+++ GO-LifeUITests/GO_LifeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GO_LifeUITestsLaunchTests.swift
+//  GO-LifeUITests
+//
+//  Created by Elliot Rapp on 8/22/24.
+//
+
+import XCTest
+
+final class GO_LifeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
